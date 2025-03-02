51062acf40c0da5d3507acaa8f0318c2e0c96499
cryham <cryham@gmail.com>
2025-03-02 01:18:39 +0100

    Rain🌧️

--- src/modules/particles/Rain.cpp
+++ src/modules/particles/Rain.cpp
@@ -1,43 +1,41 @@
-#include "Snow.h"
+#include "Rain.h"
 
-Snow::Snow(IParticleEnvironment& env) :
+Rain::Rain(IParticleEnvironment& env) :
 		Particle(env), _waterSurface(0)
 {
 	_texture = loadTexture("snow-01");
 	float s = randBetweenf(0.1f, 0.6f);
-	_scale = vec2(s, s);
+	_scale = vec2(s * 0.2f, s * 4.f);
+	_alpha = randBetweenf(0.3f, 0.5f);;
 	random();
 }
-void Snow::random () {
+void Rain::random () {
 	// _a = vec2(
 	// 	randBetweenf(-0.00001f, 0.00001f),
 	// 	randBetweenf(0.00001f, 0.00001f));
 	_v = vec2(
 		// 0.0f,
-		randBetweenf(-0.03f, 0.09f),
-		randBetweenf(0.06f, 0.12f));
+		randBetweenf(-0.01f, 0.01f),
+		randBetweenf(0.3f, 0.6f));
+	// _angle = randBetween(1, 121);
 	// _omega = 0.3f;
-	_omega = randBetweenf(-0.3f, 0.6f);
+	// _omega = randBetweenf(-0.3f, 0.6f);
 }
 
-void Snow::init() {
-	_s.x = rand() % _env.getPixelWidth();
-	// _s.y = rand() % _env.getPixelHeight();
+void Rain::init() {
 	_waterSurface = _env.getWaterSurface();
+	_s.x = rand() % _env.getPixelWidth();
 	_s.y = rand() % std::min(_waterSurface, _env.getPixelHeight());
 }
 
-void Snow::run() {
+void Rain::run() {
 	// the water height might change, so update this
 	_waterSurface = _env.getWaterSurface();
-	// const float magnitude = 0.1f;
-	// const float amplitude = 0.9f;
-	// _v.x = magnitude * sinf(_v.y * amplitude);
 
-	// snow has reached the water surface
+	// Rain has reached the water surface
 	if (_s.y >= _waterSurface - _texture->getHeight()) {
 		_s.x = rand() % _env.getPixelWidth();
-		_s.y = rand() % (_env.getPixelHeight() / 22);  // top
+		_s.y = rand() % (_env.getPixelHeight() / 32);  // top
 		random();
 	}
 }
